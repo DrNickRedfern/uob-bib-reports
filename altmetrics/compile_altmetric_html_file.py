ca77925c929895cda6dfaa5a75253241f655b57e
DrNickRedfern <nickredfernres@outlook.com>
2024-03-22 10:05:10 +0000

    altmetrics edit

--- altmetrics/compile_altmetric_html_file.py
+++ altmetrics/compile_altmetric_html_file.py
@@ -7,14 +7,13 @@ HOME_DIR = os.getcwd()
 ALTMETRICS_DIR = os.path.join(os.getcwd(), "altmetrics")
 DATA_DIR = os.path.join(HOME_DIR, 'data')
 
-with open (os.path.join(HOME_DIR, 'bibliometric_report_params.toml'), mode = 'rb') as f:
+with open(os.path.join(HOME_DIR, 'bibliometric_report_params.toml'), mode='rb') as f:
     CONFIG = tomli.load(f)
 PROJECT_NAME: str = CONFIG['project']['name']
 
 publications = os.path.join(DATA_DIR, "".join([PROJECT_NAME, "_publications_details.csv"]))
 df = pd.read_csv(publications)
-df = (
-     df
+df = (df
      .filter(['doi'])
      .drop_duplicates()
      .astype(str)
@@ -27,12 +26,11 @@ os.chdir(ALTMETRICS_DIR)
 with open('altmetric_html_top.html', 'r') as f:
     html_string = f.read()
 
-doi_html = []
 for doi in dois:
     text = ''.join(['\n<div data-condensed="true" data-badge-details="right" data-badge-type="donut" data-doi="', doi, '" data-hide-no-mentions="true" class="altmetric-embed"></div>'])
     html_string = html_string + text
 
 html_string = html_string + '\n</body>\n</html>'
 
-with open(r'altmetric_badges.html', 'w') as f:
+with open('altmetric_badges.html', 'w') as f:
         f.write(html_string)
