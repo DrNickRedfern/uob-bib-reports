da37ff377fc259683b79f50e5f412ecdb1b4ecc8
DrNickRedfern <nickredfernres@outlook.com>
2024-04-17 10:13:54 +0100

    Add internal collaborations to main python file

--- dimensions_get_data.py
+++ dimensions_get_data.py
@@ -85,7 +85,7 @@ df_researchers.to_csv(os.path.join(DATA_DIR, "".join([PROJECT_NAME, "_researcher
 dict_researchers = dict(zip(df_researchers.researcher_id, df_researchers.full_name))
 
 # * Publications: details
-# TODO Chnage the way data is collected to use publications, author, and affiliations data frames
+# TODO Change the way data is collected to use publications, author, and affiliations data frames
 # ! Do not use dsl.query_iterative here because it drops duplicates needed for accurate publications counts per researcher
 # TODO Add fields to get the data required for author positions
 print('Collecting data on publications')
@@ -376,4 +376,52 @@ df_citing_orgs_types = (
 )
 df_citing_orgs_types.to_csv(os.path.join(CITING_PUBLICATIONS, "".join([PROJECT_NAME, "_citing_org_types.csv"])), index = False)
 
+# * Internal collaboration
+publications = dsl.query_iterative(f"""search publications
+                                        where (researchers.id in {json.dumps(list(df_staff_list['researcher_id']))} and 
+                                        year in [{MIN_YEAR}:{MAX_YEAR}])
+                                        return publications[id+authors]
+                                   """)
+
+pub_df = publications.as_dataframe()
+aff_df = publications.as_dataframe_authors_affiliations()
+auth_df = publications.as_dataframe_authors()
+
+internal_authors = aff_df[aff_df['aff_id'] == GRIDID]
+internal_authors[internal_authors['researcher_id'] != '']
+
+internal_authorsf = internal_authors[internal_authors['researcher_id'] !=''][['pub_id', 'researcher_id']]
+internal_authorsf.merge(internal_authorsf, on='pub_id')
+
+edges = internal_authorsf.merge(internal_authorsf, on='pub_id').groupby(['researcher_id_x', 'researcher_id_y']).count()
+edges.reset_index(inplace = True)
+
+auth_details = auth_df.groupby('researcher_id').agg({'pub_id':'count', 'first_name':'max', 'last_name':'max'})
+# TODO Assign researcher details to id by mapping a dictionary
+auth_my_df = (
+    auth_details
+    .reset_index()
+    .assign(full_name = lambda df: df[['first_name', 'last_name']].apply(' '.join, axis = 1))
+)
+
+dict_author_id = dict(zip(auth_my_df.researcher_id, auth_my_df.full_name))
+
+my_edges_df = (
+    edges
+    .assign(
+         researcher_x = lambda df: df['researcher_id_x'].map(dict_author_id),
+         researcher_y = lambda df: df['researcher_id_y'].map(dict_author_id)
+    )
+)
+
+edges = my_edges_df
+
+new_cols = [col for col in edges.columns if col != 'pub_id'] + ['pub_id']
+edges = edges[new_cols]
+
+edges = edges[edges['researcher_id_x'] != edges['researcher_id_y']]
+
+edges.to_csv(os.path.join(DATA_DIR, "".join([PROJECT_NAME, "_edges.csv"])), index = False)
+
+# * Finished
 print('Data collection completed.')
